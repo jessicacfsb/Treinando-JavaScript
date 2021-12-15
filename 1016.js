4b6afd6bb96f38843f92a3fb000d1c982013ef05
jessicacfsb <jessica.cfsb@gmail.com>
2021-12-14 23:13:56 -0300

    exercicio 1016

--- 1016.js
+++ 1016.js
@@ -1,3 +1,7 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var dados = input.split('\n');
 
+var distanciaY = parseInt(dados.shift())
+var minutos = distanciaY * 2
+
+console.log(minutos + " minutos")
