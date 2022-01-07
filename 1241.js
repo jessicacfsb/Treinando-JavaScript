d47d32c21a7d72077b817fe3fc1807af5308845b
jessicacfsb <jessica.cfsb@gmail.com>
2022-01-07 19:02:02 -0300

    tentativa 1 exercicio 1241

---
+++ 1241.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var testar = input.split('\n');
+
+var linha1 = testar.shift().split(" ")
+var linha2 = testar.shift().split(" ")
+var linha3 = testar.shift().split(" ")
+var linha4 = testar.shift().split(" ")
+
+var n = parseInt(testar.shift())
+var aLinha1 = parseInt(aEntrada.shift())
+var bLinha1 = parseInt(bEntrada.shift())
+
+for (var i = 0; i=4 ; i--){
+    
+}
+
+
